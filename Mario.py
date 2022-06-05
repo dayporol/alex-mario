25eeed9885b09eb6eed420565b735284ab4d5b9f
Mr.Alex <syr.max.home@gmail.com>
2022-06-05 22:32:24 +0300

    добавмили простых враговб переделали стены

--- Mario.py
+++ Mario.py
@@ -1,4 +1,5 @@
 from argparse import Action
+from ast import alias
 import json
 from random import randint
 import json
@@ -13,7 +14,7 @@ text1 = f1.render(' SCORE:', True,
                   (180, 0, 0))
 
 w_world = 8000
-h_bottom = 550
+h_bottom = 540
 
 FPS = 30
 pygame.mixer.music.load("super-mario-saundtrek.mp3")
@@ -26,8 +27,9 @@ song2 = pygame.mixer.Sound("mario_jump.mp3")
 sky = pygame.image.load('sky2.jpg')
 sky_rect = sky.get_rect(bottomright=(1700, 820 ))
 block = pygame.image.load('block.jpg')
-block = pygame.transform.scale(block,(60,60))
+#block = pygame.transform.scale(block,(60,60))
 all_mario = pygame.image.load('mario.png')
+all_goomba = pygame.image.load('goomba.png')
 #all_mario = pygame.transform.scale(all_mario,(600,500))
 
 clock = pygame.time.Clock()
@@ -53,7 +55,7 @@ class WALL:
     def draw(self, sc, wx):
         if self.x - wx < w and self.x + self.w - wx > 0:
             if self.y < h_bottom:  
-                sc.blit(block, (self.x - wx, self.y + self.dy , self.w , self.h))
+                sc.blit(block, (self.x - wx, self.y + self.dy ) , (0, 0, self.w , self.h))
             else:
                 if self.w > w:
                     pygame.draw.rect(sc, GREEN, (self.x - wx, self.y , self.w , self.h))
@@ -94,7 +96,7 @@ class WORLD:
     block_size = 60
     botton_block = WALL(0, h_bottom, w_world, h - h_bottom)
     
-
+    goombas = []
     walls = []  
     stones = []  
 
@@ -113,7 +115,11 @@ class WORLD:
             data = json.load(json_file)
             self.walls.append(self.botton_block)
             for block in data['blocks']:
-                self.walls.append(WALL(block[0], block[1], self.block_size, self.block_size))
+                self.walls.append(WALL(block['x'], block['y'], block['w'], block['h']))
+        self.goombas.append(GOOMBA(530,h_bottom-46))
+        self.goombas.append(GOOMBA(630,h_bottom-46))
+        self.goombas.append(GOOMBA(730,h_bottom-46))
+        self.goombas.append(GOOMBA(830,h_bottom-46))
 
     def clean_level(self):
         self.walls.clear()
@@ -130,14 +136,30 @@ class WORLD:
             self.x_world += dx
         if dx < 0 and self.mario.x - self.x_world < w*1/4 and self.x_world > 0 :
             self.x_world += dx
+        for g in self.goombas:
+            g.move()
+        self.hit_goomba()
  
     def draw(self, sc):
         sc.blit(sky, sky_rect)
         for wall in self.walls:
             wall.draw(sc, self.x_world)
+        for g in self.goombas:
+            g.draw(sc,self.x_world)
         self.mario.draw(sc, self.x_world)
         score_text = self.font.render('score:{}'.format(self.score), True, (180, 0, 0))
         sc.blit(score_text,(10,10))
+    
+    def hit_goomba(self):
+        m = self.mario
+        for g in self.goombas:
+            if g.kill_timeout == 0:
+                self.goombas.remove(g)
+            if g.alive and not ( m.x > g.x + g.sz_x or m.x + m.sz_x < g.x or m.y + m.sz_y < g.y or m.y > g.y + g.sz_y ):
+                if m.y < g.y < m.y+m.sz_y and not m.can_jump and m.vy > 0:
+                    g.kill()
+                return True
+        return False
 
 class MARIO:
     x = 330 
@@ -203,6 +225,8 @@ class MARIO:
                 return True
         return False
 
+    def hit_goomba(self):
+        pass
 
     def set_vx(self, vx):
         self.vx = vx
@@ -221,14 +245,50 @@ class MARIO:
             self.action = "jump_left"
         
         #pygame.draw.rect(sc, BLACK, (mario.x - wx, mario.y, self.sz_x, self.sz_y))
-        #sc.blit(all_mario,(mario.x - wx, mario.y), (68, 15, self.sz, self.sz) )
         num_frames = len(self.texture[self.action])
         frame = int(self.draw_count*10/FPS)%num_frames
-        sc.blit(all_mario,(mario.x - wx, mario.y), self.texture[self.action][frame] + (self.sz_x, self.sz_y) )
+        sc.blit(all_mario,(self.x - wx, self.y), self.texture[self.action][frame] + (self.sz_x, self.sz_y) )
         if self.show_coin:
-            sc.blit(self.coin_up, (mario.x - wx, mario.y - 12))
+            sc.blit(self.coin_up, (self.x - wx, self.y - 12))
             self.show_coin -= 1
         
+
+#GOOMBA
+class GOOMBA:
+    vx = 3
+    sz_y = 46
+    sz_x = 46
+    draw_count = 0
+    alive = True
+    kill_timeout = 2*FPS
+
+    textures_walk = [(33, 11),(91,11)]
+    textures_dead = (151, 11)
+
+    def __init__(self, x, y):
+        self.x0 = x
+        self.x = x
+        self.y = y
+        
+    def kill(self):
+        self.alive = False
+
+    def move(self):
+        if self.alive:
+            self.x += self.vx
+        else:
+            self.kill_timeout -= 1
+        if abs(self.x - self.x0) > 200:
+            self.vx = - self.vx
+
+    def draw(self,sc, wx):
+        if self.alive:
+            self.draw_count += 1
+            num_frames = len(self.textures_walk)
+            frame = int(self.draw_count*10/FPS)%num_frames
+            sc.blit(all_goomba,(self.x - wx, self.y), self.textures_walk[frame] + (self.sz_x, self.sz_y) )
+        else:
+            sc.blit(all_goomba,(self.x - wx, self.y), self.textures_dead + (self.sz_x, self.sz_y) )
 
 
 mario = MARIO()
